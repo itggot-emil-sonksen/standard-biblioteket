4cd7ad30d7aec22f26a9d6072659345ae6ed2d58
itggot-emil-sonksen <emil.sonksen@itggot.se>
2018-04-18 13:44:59 +0200

    Added between_strict

---
+++ lib/between_strict.rb
@@ -0,0 +1,8 @@
+def between_strict(num1, num2, num3)
+    if num2 < num1 && num1 < num3
+        return true
+    elsif num2 > num1 && num1 > num3
+        return true
+    end
+    return false
+end
